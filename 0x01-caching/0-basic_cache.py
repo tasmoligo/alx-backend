62a750e604ca0fc7644043e6550e8056efbb114d
tasmoligo <tasieibiam28gmail.com>
2024-10-29 07:47:44 +0100

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" BasicCaching module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """Initializer. """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assigns items tothe dictionary for particular key """
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value in self.cache_data linked to key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            pass
+        return self.cache_data.get(key)
