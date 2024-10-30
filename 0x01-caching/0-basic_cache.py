390c2afadf69cdae4a156e49bf3c614a47806a90
tasmoligo <tasieibiam28gmail.com>
2024-10-30 05:53:55 +0100

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,12 +18,13 @@ class BasicCache(BaseCaching):
         """ Assigns items tothe dictionary for particular key """
         if key is None or item is None:
             pass
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
         Returns the value in self.cache_data linked to key.
         """
         if key is None or key not in self.cache_data.keys():
-            pass
+            return None
         return self.cache_data.get(key)
