c0fcbb7a2d4029b9f325097440813a337def3f9e
tasmoligo <tasieibiam28gmail.com>
2024-11-06 05:37:44 +0100

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """
+    Sets defaults
+    """
+    LANGUAGES = ["en", "fr"]
+    DEF_LOCALE = "en"
+    DEF_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello():
+    """
+    Basic flask app
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
