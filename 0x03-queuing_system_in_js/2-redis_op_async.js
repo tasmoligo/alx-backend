bfa6f2731c3109be705d3fb4f86474b3d2e9a6c0
tasmoligo <tasieibiam28gmail.com>
2024-12-05 05:00:50 +0100

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import { createClient, print } from "redis";
+import { promisify } from "util";
+
+const client = createClient();
+
+client
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", () => {
+    console.log( `Redis client not connected to the server: ${error}`)
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const value = await client.get(schoolName).catch((error) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+  console.log(value);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
